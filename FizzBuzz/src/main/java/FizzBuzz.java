4c88582d5f3178538cdd009f00f80158e2713b93
greysamuel <greysamuel@gmail.com>
2022-12-21 20:43:18 +0000

    Saved progress on FizzBuzz 2022-12-21 20:43:18 +0000

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -19,7 +19,7 @@ public class FizzBuzz {
             return "Fizz";
         } else if (n % 5 == 0) {
             return "Buzz";
-        } else if (n % 5 == 0 && n % 3 ==0) {
+        } else if (n % 3 == 0 && n % 5 == 0) {
             return "FizzBuzz";
         } else
         return "";
