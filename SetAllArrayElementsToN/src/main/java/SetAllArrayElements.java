c5745dc9373149774bfac7ea36805dd29cf91453
greysamuel <greysamuel@gmail.com>
2022-12-22 18:12:55 +0000

    Saved progress on SetAllArrayElementsToN 2022-12-22 18:12:55 +0000

--- SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
+++ SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
@@ -24,7 +24,7 @@ public class SetAllArrayElements {
      */
     public void set(int[] arr, int n){
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            arr[i] =  n;
         }
     }
 }
