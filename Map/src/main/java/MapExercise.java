0b712f47d9c40a418e87881d390ee178d61e2c6c
greysamuel <greysamuel@gmail.com>
2022-12-30 18:55:54 +0000

    Saved progress on Map 2022-12-30 18:55:54 +0000

--- Map/src/main/java/MapExercise.java
+++ Map/src/main/java/MapExercise.java
@@ -48,6 +48,7 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addKeyValuePair(Map<Integer,String> map, int key, String value){
+        map.put(key, value);
     }
 
     /**
