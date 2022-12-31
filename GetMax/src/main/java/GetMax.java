da238acd09c7bd1c1dd04c17be72b8781759ef72
greysamuel <greysamuel@gmail.com>
2022-12-31 02:33:05 +0000

    Saved progress on GetMax 2022-12-31 02:33:05 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -9,8 +9,8 @@ public class GetMax {
     public int max(int[] arr){
         int maxNumber = 0;
         for(int i = 0; i < arr.length; i++){
-            if (arr[i] > maxNumber)
-            arr[i] = maxNumber;
+            if (arr[i] > arr[maxNumber]) 
+            i = maxNumber;
         }
         return maxNumber;
     }
