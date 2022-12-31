4884694699bcf7934a04b08ff411405d70dc9101
greysamuel <greysamuel@gmail.com>
2022-12-31 01:11:20 +0000

    Saved progress on ArraySum 2022-12-31 01:11:20 +0000

--- ArraySum/src/main/java/ArraySum.java
+++ ArraySum/src/main/java/ArraySum.java
@@ -7,7 +7,9 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
-
-        return 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]++;
+        }
+        return sum(arr);
     }
 }
