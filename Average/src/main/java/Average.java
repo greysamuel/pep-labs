9715b767d59c02ec0e93f9bf4d85ef13adf56abc
greysamuel <greysamuel@gmail.com>
2022-12-31 02:11:38 +0000

    Saved progress on Average 2022-12-31 02:11:38 +0000

--- Average/src/main/java/Average.java
+++ Average/src/main/java/Average.java
@@ -20,9 +20,9 @@ public class Average {
     public double getAvg(int[] nums){
         double avg = 0;
         for (int i = 0; i <nums.length; i++){
-            avg = (double)nums[i];
+            avg = avg + (double)nums[i]/nums.length;
             
         }
-        return avg/nums.length;
+        return avg;
     }
 }
