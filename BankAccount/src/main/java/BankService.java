fdb030b20ba2131a2b64c2b298b2dd9ff45a41ab
greysamuel <greysamuel@gmail.com>
2023-01-04 01:32:18 +0000

    Saved progress on BankAccount 2023-01-04 01:32:18 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -23,7 +23,10 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        if (amount >= 0){
+            balance += amount;
+        }
+        
     }
 
     /**
@@ -32,7 +35,9 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if(amount >= 0 && amount <= balance){
+            balance -= amount;
+        }
     }
 
     /**
@@ -40,6 +45,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
