c2567dc552bfc11e6e68ca077453f5bcacc1ecb8
greysamuel <greysamuel@gmail.com>
2023-01-10 15:51:25 +0000

    Saved progress on MaxMinusMin 2023-01-10 15:51:25 +0000

--- MaxMinusMin/src/main/java/MaxMinusMin.java
+++ MaxMinusMin/src/main/java/MaxMinusMin.java
@@ -10,6 +10,19 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        if(nums.isEmpty()){
+            return 0;
+        }else {
+            int max = nums.get(0);
+            int min = nums.get(0);
+            for (int i: nums){
+                if (i > max) {
+                    max = i;
+                } else if (i < min){
+                    min = i;
+                }
+            }return (max - min)+ 1;
+        }
+        // return (max - min)+ 1;
     }
 }
