3d1af443c4e8023d43c7424e0f2e979776d2673b
greysamuel <greysamuel@gmail.com>
2023-01-28 21:32:10 +0000

    Saved progress on CombineArrays 2023-01-28 21:32:10 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class CombineArrays {
     /**
      * Combine two arrays into a single array and return it.
@@ -8,7 +8,12 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int a1 = arr1.length;
+        int a2 = arr2.length;
+        int c1 = a1 + a2;
+        int[] c = new int[c1];
+        System.arraycopy(arr1, 0, c, 0, a1);
+        System.arraycopy(arr2, 0, c, a1, a2);
+        return c;
     }
 }
