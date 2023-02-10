8edbb9df7e5c6bbb4bb83a12d71f004655a46666
greysamuel <greysamuel@gmail.com>
2023-02-10 19:12:56 +0000

    Saved progress on CharComparison 2023-02-10 19:12:56 +0000

--- CharComparison/src/main/java/CharComparison.java
+++ CharComparison/src/main/java/CharComparison.java
@@ -1,34 +1,30 @@
 public class CharComparison {
-    /**
-     * Given an array of char, determine if the array A belongs to the left or to the right of char array B lexigraphically.
-     * That means its position in a dictionary, eg:
-     * cat < dog < mouse
-     * cat < car < care
-     * You will need to compare an index of both arrays against each other as you iterate with a for loop.
-     *
-     * @param a an array of char.
-     * @param b an array of char.
-     * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
-     */
-    public int compare(char[] a, char[] b){
-        int i = 0;
-    
-    for(;i <a.length && i <b.length;){
-      if(a[i] < b[i]){
-        return  -1;
-      }else if(a[i] > b[i]){
-        return  1;
-      }
-    }
-      if(i == a.length && i == b.length){
-        return 0;
-      }else if(i == a.length){
-        return -1;
-      }else{
-        return 1;
-      }
+  /**
+   * Given an array of char, determine if the array A belongs to the left or to
+   * the right of char array B lexigraphically.
+   * That means its position in a dictionary, eg:
+   * cat < dog < mouse
+   * cat < car < care
+   * You will need to compare an index of both arrays against each other as you
+   * iterate with a for loop.
+   *
+   * @param a an array of char.
+   * @param b an array of char.
+   * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two
+   *         arrays are identical.
+   */
+  public int compare(char[] a, char[] b) {
+    int i = 0;
 
-    
-        
+    for (; i < a.length && i < b.length;) {
+      if (a[i] < b[i]) {
+        return -1;
+      } else if (a[i] > b[i]) {
+        return 1;
+      } else if (a[i] == b[i]) {
+        return 0;
+      }rev
     }
+    return 0;
+  }
 }
