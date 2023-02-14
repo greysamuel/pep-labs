17a2d6710e7342b69ac96b9bcd24c2abf14d4ad5
greysamuel <greysamuel@gmail.com>
2023-02-14 20:05:49 +0000

    Saved progress on StringIsPalindrome 2023-02-14 20:05:49 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -12,6 +12,14 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        return false;
+        String reverse = "";
+        Boolean answer = false;
+        for (int i = str.length() - 1; i >= 0; i--){
+            reverse = reverse + str.charAt(i);
+        }
+        if (str.equals(reverse)){
+            answer = true;
+        }
+        return answer;
     }
 }
